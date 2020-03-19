ee056d41ccda1413464912b7428f5ffb89dcdb70
Ian Goh <ian.goh.y@gmail.com>
2020-03-19 09:18:12 +0800

    Changed displaying of gameboard to use recursive technique instead of hardcoding

--- src/index.js
+++ src/index.js
@@ -18,25 +18,21 @@ class Board extends React.Component {
       />);
   }
 
+  makeBoard() {
+    let board = []
+    for (let x=0; x<3; x++) {
+      let square = []
+      for (let i=0; i<3; i++) {
+        square = square.concat(<>{this.renderSquare(x*3 + i)}</>)
+      }
+      board = board.concat(<div className="board-row">{square}</div>)
+    }
+    return board;
+  }
   render() {
+    const board = this.makeBoard();
     return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      board
     );
   }
 }
