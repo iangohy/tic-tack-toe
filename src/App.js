7b704c215f7fc87712245d3f5e6309a3f2e9db4a
Ian <ian@iangohy.com>
2020-03-21 08:25:04 +0800

    Restructed app for future development. Added title and decorations to status text.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Game } from './game';
+
+export default function App() {
+  return (
+    <>
+    <h1> Challenge your friend to a game of Tic-Tack-Toe!</h1>
+    <Game />
+    </>
+  )
+}
