12ecb08c50df3f25d707d3b98cb0eee5b84243f7
Ian <ian@iangohy.com>
2020-03-25 15:55:12 +0800

    Updates

---
+++ precache-manifest.8dc0b714637f0c2487d5a722b82d0494.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88fd86f254392c0758d5090a398e848f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84d00b0b85590dc1a1f9",
+    "url": "/static/css/main.722a68fb.chunk.css"
+  },
+  {
+    "revision": "a66f81789aef3099299b",
+    "url": "/static/js/2.138fd8a1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.138fd8a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84d00b0b85590dc1a1f9",
+    "url": "/static/js/main.cef8c769.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
